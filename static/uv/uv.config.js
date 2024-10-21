1c32c37dc1174c61065abcb2a0916f8ecb2c8b89
willo <153154914+willowastaken@users.noreply.github.com>
2024-10-20 22:07:03 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/static/pa/',
-  bare: 'https://pabare.lan-mb.si/bare/',  
+  bare: 'https://pabare.lgbtq.tel/bare/',  
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/static/uv/uv.handler.js',
